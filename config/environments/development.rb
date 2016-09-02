2aebc8ca8fd550e8c59ab5fef68f0f14c09645cc
Takahito Iwase <hito.0525@gmail.com>
2016-09-02 18:09:58 +0900

    dive13 gitignoreを使って余分なファイルを追加しない

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,10 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  #better_errorsを使用することで、エラー画面を便利に
+   BetterErrors::Middleware.allow_ip! "0.0.0.0/0"
+
+
+
 end
